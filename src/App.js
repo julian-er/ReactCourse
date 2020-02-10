b7ee601986078398b44abe5483e5f29b8e53c9ce
Julián Rosalen <julianrosalen@MacBookdeJulian.local>
2020-02-10 15:07:46 -0300

    components and simple status

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Components/Person'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    person: [
+        {
+          name:"Componente",
+          age:"tengo 1 dia de creado"
+        },
+        {
+          name:"Componentesito",
+          age:"tengo 1 ratin"
+        },
+        {
+          name:"Componente mayor",
+          age:"tengo 1 mes de creado"
+        }
+    ]
+    
+  }
+
+  switchPersonHandler = () =>{
+
+    if (!this.state.switchedName){
+      this.setState({
+        name:"Componentesito",
+        age:"soy recién nacido"
+      })
+    }
+    console.log(this.state.switchedName)
+  
+  }
+
+  // handleCreatePerson = () => {
+
+  //   this.state.person.map( () => {  <Person name={this.state.name} age={this.state.age}/> }
+  //   )
+
+  // }
+
+render(){
+
+          return (
+            <div className="App">
+              <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                {this.handleCreatePerson} <button onClick={this.switchPersonHandler}>Switch Name</button>
+              </header>
+              <Person name={this.state.person[0].name} age={this.state.person[0].age}/>
+              <Person name={this.state.person[1].name} age={this.state.person[1].age}/>
+              <Person name={this.state.person[2].name} age={this.state.person[2].age}/>
+            </div>
+          );
+        }
 }
 
 export default App;
---
+++ src/Components/Person.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+const person = (props) => {
+
+return <div>
+
+<p>¡Hola! soy {props.name} y {props.age}</p>
+
+
+
+</div>
+
+
+
+
+}
+
+export default person
